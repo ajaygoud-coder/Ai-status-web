963d68d2cb5be24f08e14c9a3da32d2534750234
Pochagoni ajaygoud <118655290+ajaygoud-coder@users.noreply.github.com>
2025-11-27 12:06:32 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      height:"100vh",
+      fontSize:"32px",
+      fontWeight:"bold"
+    }}>
+      AI Status Web is Live 🚀
+    </div>
+  );
+}
